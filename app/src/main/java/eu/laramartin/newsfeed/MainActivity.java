ef15430b688efe1650878b18d2df3db4c6094b42
Lara Martin <lara.martin.carretero@gmail.com>
2016-09-20 18:33:22 +0200

    create url with hardcoded parameters to test

--- app/src/main/java/eu/laramartin/newsfeed/MainActivity.java
+++ app/src/main/java/eu/laramartin/newsfeed/MainActivity.java
@@ -1,7 +1,9 @@
 package eu.laramartin.newsfeed;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v("Mainactivity", "URL: " + createURL());
     }
+
+    private String createURL() {
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("http")
+                .encodedAuthority("content.guardianapis.com")
+                .appendPath("search")
+                .appendQueryParameter("order-by", "newest")
+                .appendQueryParameter("show-references", "author")
+                .appendQueryParameter("q", "tokyo")
+                .appendQueryParameter("api-key", "test");
+        String url = builder.build().toString();
+        return url;
+    }
+
 }
